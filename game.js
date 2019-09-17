4f9cd02ac0cade1f1342c19dea67e96928008cc6
Philip <pvlevc59@harrisonburg.k12.va.us>
2019-09-17 14:12:36 -0400

    Update game.js

--- game.js
+++ game.js
@@ -4,10 +4,18 @@ postToPage(score); // Print to the page
 
 createButton('Button', btnPress); // Make a button
 changeTitle('Clicker Game');
+createButton('Button2.0', botnPress);
 
 setInterval(btnPress, 1000);
+setInterval(botnPress, 1000);
 
 function btnPress() {
   score++;
   postToPage(score);
 }
+
+function botnPress() {
+  score--;
+  postToPage(score);
+}
+if(score > 20) 
