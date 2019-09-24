19bf869bd642ff4865c8e00a2e09ae686b35a764
Philip <pvlevc59@harrisonburg.k12.va.us>
2019-09-24 14:29:55 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ var score = 1; // Create a variable to store the score in
 
 var title = new Title('You are the Pope'); // Create a title element
 var button = new Button('Call a Crusade', btnPress);   // Create a button element
-var button2 = new Button('⚔️ 🛡️', botnPress);
+var button2 = new Button('Recruit units', botnPress);
 var hidden = new Button('England has joined the Crusade');
 hidden.hide();
 var France = new Button('France has joined the Crusade');
@@ -11,11 +11,13 @@ var power = new Button('Balance of power');
 power.hide();
 var scr = new Text(score);                    // Create a text element
 
+var text1 = new Text('Units on the field');
+
 setInterval(btnPress, 1000);   // Set up a loop
 setInterval(botnPress, 1000);
 
 function botnPress() {
-  score--;
+  score++;
 }
 function btnPress() {
   score++;         // Increase the score
@@ -25,6 +27,15 @@ function btnPress() {
   }
 
   scr.edit(score); // Update the page with the new score
+  if(score > 1) {
+    button.hide();
+  }
+
+  scr.edit(score); // Update the page with the new score
+  if(score > 2) {
+    France.show();
+  }
+  scr.edit(score); // Update the page with the new score
   if(score > 2) {
     France.show();
   }
@@ -33,4 +44,15 @@ function btnPress() {
   if(score > 2) {
     power.show();
   }
+
+//England hide code
+  scr.edit(score);
+  if(score > 10) {
+    hidden.hide();
+  }
+//France hide code
+scr.edit(score);
+if(score > 10) {
+  France.hide();
+}
 }
