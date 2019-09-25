41863077b00ce002112bf94e637f963f6822c9f3
Philip <pvlevc59@harrisonburg.k12.va.us>
2019-09-25 14:30:19 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,13 +1,13 @@
 var score = 1; // Create a variable to store the score in
 
-var title = new Title('You are the Pope'); // Create a title element
-var button = new Button('Call a Crusade', btnPress);   // Create a button element
-var button2 = new Button('Recruit units', botnPress);
+var title = new Title('You are the Pope'); // Create a title element   // Create a button element
+var button2 = new Button('Recruit Units', btnPress);
+var button = new Button('Call a Crusade', botnPress);
 var hidden = new Button('England has joined the Crusade');
 hidden.hide();
 var France = new Button('France has joined the Crusade');
 France.hide();
-var power = new Button('Balance of power');
+var power = new Button('Battles won');
 power.hide();
 var scr = new Text(score);                    // Create a text element
 
@@ -31,10 +31,6 @@ function btnPress() {
     button.hide();
   }
 
-  scr.edit(score); // Update the page with the new score
-  if(score > 2) {
-    France.show();
-  }
   scr.edit(score); // Update the page with the new score
   if(score > 2) {
     France.show();
